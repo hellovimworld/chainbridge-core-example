48f0aa9ba98e68fada403566f4cd989b09b4193e
ob <ob.haung@vimworld.com>
2023-03-15 16:13:56 +0800

    workflows test

--- example/app.go
+++ example/app.go
@@ -136,3 +136,18 @@ func bugFun() {
 	fmt.Println("Value of num is: ", num)
 	fmt.Println("Value of val is: ", val)
 }
+
+
+func bugFun2() {
+	var num int = 5
+	var ptr *int
+	var val int
+
+	ptr = &num
+
+	val = *ptr + 2
+
+	fmt.Println("Value of num is: ", num)
+	fmt.Println("Value of val is: ", val)
+}
+
